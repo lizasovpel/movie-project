cb784df3efd784706f95495a99326352a0217269
Elizabeth <elizabethsovpel@MacBook-Pro-Elizabeth.local>
2022-04-07 12:05:09 +0300

    pages

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -10,24 +10,52 @@ const Pagination = () => {
 	useEffect(() => {
 		console.log(page);
 	}, [page]);
+	const disabled = page === 1 ? "page-item disabled" : "page-item";
 
 	return (
-		<div className="pagination">
-			<div className="buttons">
-				<button
-					type="button"
-					className="btn btn-outline-light w100"
-					style={page === 1 ? { visibility: "hidden" } : null}
-					onClick={() => dispatch(pageChange(-1))}
-				>
-					prev
-				</button>
-				<div className="pageNumber">{page}</div>
-				<button type="button" className="btn btn-outline-light w100" onClick={() => dispatch(pageChange(1))}>
-					next
-				</button>
-			</div>
-		</div>
+		<nav>
+			<ul className="pagination justify-content-center">
+				<li className={disabled}>
+					<button className="page-link" onClick={() => dispatch(pageChange(-1))}>
+						<span aria-hidden="true">&laquo;</span>
+					</button>
+				</li>
+				<li className="page-item ">
+					<button className="page-link act">{page}</button>
+				</li>
+				<li className="page-item">
+					<button className="page-link" onClick={() => dispatch(pageChange(1))}>
+						{page + 1}
+					</button>
+				</li>
+				<li className="page-item">
+					<button className="page-link" onClick={() => dispatch(pageChange(2))}>
+						{page + 2}
+					</button>
+				</li>
+				<li className="page-item">
+					<button className="page-link" onClick={() => dispatch(pageChange(1))}>
+						<span aria-hidden="true">&raquo;</span>
+					</button>
+				</li>
+			</ul>
+		</nav>
+		// <div className="pagination">
+		// 	<div className="buttons">
+		// 		<button
+		// 			type="button"
+		// 			className="btn btn-outline-light w100"
+		// 			style={page === 1 ? { visibility: "hidden" } : null}
+		// 			onClick={() => dispatch(pageChange(-1))}
+		// 		>
+		// 			&laquo;
+		// 		</button>
+		// 		<div className="pageNumber">{page}</div>
+		// 		<button type="button" className="btn btn-outline-light w100" onClick={() => dispatch(pageChange(1))}>
+		// 			&laquo;
+		// 		</button>
+		// 	</div>
+		// </div>
 	);
 };
 
