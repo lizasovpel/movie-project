c1ee33baf60926b13aa68f1c46fef978e2fff5dd
Elizabeth <elizabethsovpel@MacBook-Pro-Elizabeth.local>
2022-04-13 12:17:34 +0300

    main

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,28 +1,21 @@
 import { useCallback } from "react";
 
 export const useHttp = () => {
-    const request = useCallback(
-        async (
-            url,
-            method = "GET",
-            body = null,
-            headers = { "Content-Type": "application/json" }
-        ) => {
-            try {
-                const res = await fetch(url, { method, body, headers });
-                if (!res.ok) {
-                    throw new Error(
-                        `could not fetch ${url}, status: ${res.status}`
-                    );
-                }
-                const data = await res.json();
-                return data;
-            } catch (error) {
-                throw error;
-            }
-        },
-        []
-    );
+	const request = useCallback(
+		async (url, method = "GET", body = null, headers = { "Content-Type": "application/json" }) => {
+			try {
+				const res = await fetch(url, { method, body, headers });
+				if (!res.ok) {
+					throw new Error(`could not fetch ${url}, status: ${res.status}`);
+				}
+				const data = await res.json();
+				return data;
+			} catch (error) {
+				throw error;
+			}
+		},
+		[]
+	);
 
-    return { request };
+	return { request };
 };
