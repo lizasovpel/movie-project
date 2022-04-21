c29fcf342c7d6952440b049ccfe921da274ba573
Elizabeth <elizabethsovpel@MacBook-Pro-Elizabeth.local>
2022-04-21 09:30:09 +0300

    first commit

---
+++ src/components/genres/Genres.js
@@ -0,0 +1,36 @@
+import "./Genres.sass";
+
+const Genres = () => {
+	return (
+		<div className="Container">
+			<h2>Genres</h2>
+			<div className="genres">
+				<button type="button" className="btn btn-outline-light">
+					action
+				</button>
+				<button type="button" className="btn btn-outline-light">
+					comedy
+				</button>
+				<button type="button" className="btn btn-outline-light">
+					drama
+				</button>
+				<button type="button" className="btn btn-outline-light">
+					fantasy
+				</button>
+				<button type="button" className="btn btn-outline-light">
+					horror
+				</button>
+				<button type="button" className="btn btn-outline-light">
+					mystery
+				</button>
+				<button type="button" className="btn btn-outline-light">
+					romance
+				</button>
+				<button type="button" className="btn btn-outline-light">
+					thriller
+				</button>
+			</div>
+		</div>
+	);
+};
+export default Genres;
