58a025146874fabe9bf413d500fffa1a05fad8b4
Elizabeth <elizabethsovpel@MacBook-Pro-Elizabeth.local>
2022-05-24 11:50:55 +0300

    password error

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -19,55 +19,50 @@ const SignIn = () => {
 		const users = JSON.parse(localStorage.getItem("users"));
 		const loginHelp = document.querySelector("#loginHelp");
 		const passwordHelp = document.querySelector("#passwordHelp");
-		const wrongLogin = document.querySelector("#wrongLogin");
 		const wrongPassword = document.querySelector("#wrongPassword");
 
 		if (loginValue && passwordValue) {
 			loginHelp.hidden = true;
 			passwordHelp.hidden = true;
-			wrongLogin.hidden = true;
 			wrongPassword.hidden = true;
 			const accountExists = users.find((user) => user.login === loginValue);
-			if (accountExists) {
-				if (passwordValue !== accountExists.password) {
-					wrongPassword.hidden = false;
-				} else {
-					localStorage.setItem("username", loginValue);
-					localStorage.setItem("password", passwordValue);
-					await request(
-						`https://api.themoviedb.org/3/authentication/token/new?${process.env.REACT_APP_KEY}`
-					).then((data) => localStorage.setItem("token", data.request_token));
-					window.open(`https://www.themoviedb.org/authenticate/${localStorage.getItem("token")}`);
-					const postfunc = async () => {
-						clearTimeout(postfunc);
-						await postRequest(
-							`https://api.themoviedb.org/3/authentication/session/new?${process.env.REACT_APP_KEY}`,
-							{
-								"request_token": localStorage.getItem("token"),
-							}
-						)
-							.then((res) => {
-								localStorage.setItem("session_id", res.data.session_id);
-							})
-							.catch((error) => setTimeout(postfunc, 500));
-						await request(
-							`https://api.themoviedb.org/3/account?${
-								process.env.REACT_APP_KEY
-							}&session_id=${localStorage.getItem("session_id")}`
-						)
-							.then((res) => {
-								localStorage.setItem("username", res.username);
-								localStorage.setItem("id", res.id);
-								// dispatch(userLoggedIn);
-								dispatch(activeGenreChanged("all"));
-							})
-							.then(() => navigate("/"))
-							.catch((error) => console.log(error));
-					};
-					setTimeout(postfunc, 1000);
-				}
+			console.log(accountExists);
+			if (!accountExists || passwordValue !== accountExists.password) {
+				wrongPassword.hidden = false;
 			} else {
-				wrongLogin.hidden = false;
+				localStorage.setItem("username", loginValue);
+				localStorage.setItem("password", passwordValue);
+				await request(
+					`https://api.themoviedb.org/3/authentication/token/new?${process.env.REACT_APP_KEY}`
+				).then((data) => localStorage.setItem("token", data.request_token));
+				window.open(`https://www.themoviedb.org/authenticate/${localStorage.getItem("token")}`);
+				const postfunc = async () => {
+					clearTimeout(postfunc);
+					await postRequest(
+						`https://api.themoviedb.org/3/authentication/session/new?${process.env.REACT_APP_KEY}`,
+						{
+							"request_token": localStorage.getItem("token"),
+						}
+					)
+						.then((res) => {
+							localStorage.setItem("session_id", res.data.session_id);
+						})
+						.catch((error) => setTimeout(postfunc, 500));
+					await request(
+						`https://api.themoviedb.org/3/account?${
+							process.env.REACT_APP_KEY
+						}&session_id=${localStorage.getItem("session_id")}`
+					)
+						.then((res) => {
+							localStorage.setItem("username", res.username);
+							localStorage.setItem("id", res.id);
+							// dispatch(userLoggedIn);
+							dispatch(activeGenreChanged("all"));
+						})
+						.then(() => navigate("/"))
+						.catch((error) => console.log(error));
+				};
+				setTimeout(postfunc, 1000);
 			}
 		} else {
 			loginValue ? (loginHelp.hidden = true) : (loginHelp.hidden = false);
@@ -88,9 +83,6 @@ const SignIn = () => {
 						<div id="loginHelp" className="form-text" hidden>
 							enter login
 						</div>
-						<div id="wrongLogin" className="form-text" hidden>
-							user doesn't exist
-						</div>
 					</div>
 					<div className="mb-3">
 						<label htmlFor="passwordInput" className="form-label">
@@ -101,7 +93,7 @@ const SignIn = () => {
 							enter password
 						</div>
 						<div id="wrongPassword" className="form-text" hidden>
-							wrong password
+							wrong login or password
 						</div>
 					</div>
 					<button type="submit" className="btn">
